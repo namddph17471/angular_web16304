895d483fe0c8ddb943399a28fa9e18edfa02813e
namddph17471 <namddph17471@fpt.edu.vn>
2022-05-11 09:34:50 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_we16304';
+  title = 'angular_we16304 test';
+  // Phần Logic định nghĩa giá trị biến và sử dụng ở html
+  name = 'Trương';
+  class = 'WEB16304';
+  // kiểu mảng
+  students = [
+    {
+      id:'PH0001',
+      name:'vothuy',
+      status:0
+    },
+    {
+      id:'PH0002',
+      name:'vothuy2',
+      status:1
+    },
+    {
+      id:'PH0003',
+      name:'vothuy3',
+      status:0
+    }
+  ]
 }
