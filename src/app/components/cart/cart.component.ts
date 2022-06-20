a17eb43dfa9c55eb2f36b1ad1dbdafb1ed836252
namddph17471 <namddph17471@fpt.edu.vn>
2022-06-20 08:56:09 +0700

    update

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -16,10 +16,7 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     this.onSetCartItems();
-    // Cần 1 cách thức nào đó có thể lắng nghe việc thay đổi giá trị của ls
-    // Hoặc cho biết khi nào có thể lấy dữ liệu mới
     this.lsService.watchService().subscribe(data => {
-      // Khi serviceSubject.next('') thì subscribe sẽ được gọi
       this.onSetCartItems();
     });
   }
